09cce67ae8d1d499579afeaf90145e812db17711
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 17:53:37 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../components/register-components';
 import '../css/main.css';
 import Head from 'next/head';
+import 'stackbit-typist/dist/Typist.css';
 
 export default function MyApp({ Component, pageProps }) {
     return (
